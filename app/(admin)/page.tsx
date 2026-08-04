f7f67248da1f513fca27206819595ce00bab8e57
denis-mahei <denmahei@gmail.com>
2026-08-04 21:02:27 +0300

    refactor: redirect root to dashboard

--- app/(admin)/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DashboardPage from "@/app/(admin)/dashboard/page";
-
-function Page() {
-  return <DashboardPage />;
-}
-
-export default Page;
--- proxy.ts
+++ proxy.ts
@@ -18,6 +18,5 @@ export const config = {
     "/customers",
     "/suppliers",
     "/products/:path*",
-    "/",
   ],
 };
