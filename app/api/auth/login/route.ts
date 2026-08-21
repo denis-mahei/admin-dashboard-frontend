024398e17789f6562cb05cbbdd4ba9f39d617ee6
denis-mahei <denmahei@gmail.com>
2026-08-21 18:27:19 +0300

    feat: added route handler for login/logout

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import { apiServer } from "@/lib/api/api.server";
+import { cookies } from "next/headers";
+import { parseCookie } from "cookie";
+import axios from "axios";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  try {
+    const res = await apiServer.post("/auth/login", body);
+
+    const cookieStore = await cookies();
+    const setCookie = res.headers["set-cookie"];
+
+    if (setCookie) {
+      const cookieArr = Array.isArray(setCookie) ? setCookie : [setCookie];
+
+      for (const cookieStr of cookieArr) {
+        const parsed = parseCookie(cookieStr);
+        const options = {
+          httpOnly: true,
+          sameSite: "lax" as const,
+          secure: process.env.NODE_ENV === "production",
+          path: parsed.Path || "/",
+        };
+        if (parsed.access_token) {
+          cookieStore.set("access_token", parsed.access_token, options);
+        }
+        console.log("My options:", options);
+      }
+      return NextResponse.json(res.data);
+    }
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  } catch (e) {
+    if (axios.isAxiosError(e)) {
+      return NextResponse.json(
+        { message: e?.response?.data.message },
+        { status: e?.response?.status },
+      );
+    }
+  }
+}
---
+++ app/api/auth/logout/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { apiServer } from "@/lib/api/api.server";
+
+export async function GET() {
+  try {
+    await apiServer.get("/auth/logout");
+  } catch (e) {
+    console.log("NestJS logout failed:", e);
+  }
+
+  const cookieStore = await cookies();
+  cookieStore.delete("access_token");
+
+  return NextResponse.json({ success: true });
+}
